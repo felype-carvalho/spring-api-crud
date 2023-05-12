e85d324564be8bcda5228e0dde4f04ec38e3eeaa
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-05-12 20:06:42 -0300

    Initialize spring project

---
+++ crud-spring-api/src/main/java/com/crudspringapi/crudspringapi/CrudSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.crudspringapi.crudspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApiApplication.class, args);
+	}
+
+}
